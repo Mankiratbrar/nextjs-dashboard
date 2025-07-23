849f8f300a1d8839e753bc75ace476c440174971
Mankirat Singh <smankirat6u@gmail.com>
2025-07-23 15:57:59 +0530

    w

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,8 @@ import {z} from 'zod';
 import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!,{ssl:require});
 
@@ -100,3 +102,25 @@ export async function deleteInvoice(id:string) {
     revalidatePath('/dashboard/invoices');
 }
 
+export async function authenticate(
+    prevState: String|undefined,
+    formData : FormData
+) {
+    try {
+        await signIn('credentials',formData)
+    }catch(error){
+        if(error instanceof AuthError)
+        {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return ('Invaild login details')
+                    break;
+            
+                default:
+                    return('Something is wrong')
+                    break;
+            }
+        }
+        throw error
+    }
+}
